09fa1b4743cf98b4b6d18dfa8cb0348e6a55a815
tenochg <yomoozuki@gmail.com>
2023-02-04 10:21:00 -0800

    Assignment 1

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
+    num_males = int(input('Number of Males in class: '))
+    num_females = int(input('Number of Females in class: '))
+    total_students = num_males + num_females
+    percentage_males = (num_males/total_students) * 100
+    percentage_females = (num_females/total_students) * 100
+    print(f'The total number of students:{total_students:>15}')
+    print(f'The Number of Males and Females: {num_males:>10} {num_females:>10}')
+    print(f'The Percentage of Males and Females: {percentage_males:>9.2f}%{percentage_females:>9.2f}%')
     pass
 
 
